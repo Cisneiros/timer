848ba463511f701cb8a74a437d97d8fa7b67a427
Alexandre Cisneiros <alexandre.cisneiros@nubank.com.br>
2020-08-17 13:43:40 -0300

    Publish

---
+++ precache-manifest.2cda7cef34402bade42ce0da41be9e52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8178d824103f09023e2931822a5ca2a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45561415843da831016c",
+    "url": "/static/css/main.0314dbf9.chunk.css"
+  },
+  {
+    "revision": "443e06adf4c16e9a8d8c",
+    "url": "/static/js/2.ba029ee7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ba029ee7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45561415843da831016c",
+    "url": "/static/js/main.93f3676e.chunk.js"
+  },
+  {
+    "revision": "89ac35b4ba8ca899f16a",
+    "url": "/static/js/runtime-main.7f7132dd.js"
+  }
+]);
